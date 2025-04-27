e56dc13b32385c8b6a5a4d91471afe9ea6927102
Jim Liu <jim.t90615@gmail.com>
2025-04-27 23:30:08 +0800

    stm8: enable rc522
    Signed-off-by: Jim Liu <jim.t90615@gmail.com>

--- main.c
+++ main.c
@@ -6,6 +6,37 @@
 
 #include "stm8s.h"
 #include "stm8s_conf.h"
+#include "string.h"
+#include "uart.h"
+#include "rc522.h"
+
+void Delay(u32 nCount);
+extern u8 RxBuffer[RxBufferSize];
+extern u8 UART_RX_NUM;
+
+unsigned char CT[2];//卡?型
+unsigned char SN[4]; //卡?
+
+unsigned char write[16] = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10};
+unsigned char read[16] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+unsigned char key[6] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+
+/* Private macro -------------------------------------------------------------*/
+#define countof(a) (sizeof(a) / sizeof(*(a)))
+#define  BufferSize (countof(Tx_Buffer)-1)
+/* Private variables ---------------------------------------------------------*/
+u8 Tx_Buffer[] = "STM8S RFID TEST: RFID test";
+u8 Rx_Buffer[BufferSize];
+u32 FLASH_ID ;
+/* Private defines -----------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+void cardNo2String(u8 *cardNo, u8 *str);
+/* Private functions ---------------------------------------------------------*/
+
+
+u8 RxBuffer[RxBufferSize];
+u8 UART_RX_NUM=0;
 
 void Clock_Config(void)
 {
@@ -18,20 +49,98 @@ void Clock_Config(void)
 	//set HSI DIV(High speed internal clock prescaler: 1)
 	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
 
-	//enable i2c function
-	//CLK_PeripheralClockConfig(CLK_PERIPHERAL_I2C, ENABLE);
 }
 
 main()
 {
+	unsigned char status;
 	Clock_Config();
 	GPIO_Init(GPIOA, GPIO_PIN_3, GPIO_MODE_OUT_PP_LOW_FAST);
 	MX_TIM4_Init();
+	status = memcmp(read,write,16);
+	Uart_Init();
+	InitRc522();
 	rim();
+	UART1_SendString(Tx_Buffer,BufferSize);
+	
 
 	while (1) {
-		Delay_ms_int(100);
-		GPIO_WriteReverse(GPIOA, GPIO_PIN_3);
+		status = PcdRequest(PICC_REQALL,CT);      /*?描卡*/
+    status = PcdAnticoll(SN);                 /*防?撞*/      
+    if (status==MI_OK)
+    {
+        GPIO_LOW(GPIOA, GPIO_PIN_3);   
+				UART1_SendString("The card Id iszzz:",15); 
+        cardNo2String(SN, Tx_Buffer);
+        UART1_SendString(Tx_Buffer, 17);        
+        
+        Reset_RC522();
+    }
+    else
+    {
+        GPIO_HIGH(GPIOA, GPIO_PIN_3);
+    }   
 	}
 
 }
+
+void Delay(u32 nCount)
+{
+  /* Decrement nCount value */
+  while (nCount != 0)
+  {
+    nCount--;
+  }
+}
+
+void Hex2String(u8 hex,u8 *str)
+{
+  str[0] = (hex / 100) + '0';
+  str[1] = (hex % 100 / 10) + '0';
+  str[2] = (hex % 10) + '0';
+}
+
+void cardNo2String(u8 *cardNo, u8 *str)
+{
+    u8 Count = 0;
+    for(Count = 0; Count < 4; Count++)
+    {
+        Hex2String(cardNo[Count], str + Count * 4);
+        if(Count == 3)
+        {
+          str[15] = '\n';
+        }
+        else
+        {
+          str[Count * 4 + 3] = ':';
+        }
+    }
+}
+/*
+@far @interrupt void UART1_RX_IRQHandler(void)
+{
+	  u8 Res;
+    if(UART1_GetITStatus(UART1_IT_RXNE )!= RESET)  
+    {
+			Res =UART1_ReceiveData8();
+			if(( UART_RX_NUM&0x80)==0)
+			{
+				if( UART_RX_NUM&0x40)
+				{
+					if(Res!=0x0a) UART_RX_NUM=0;
+					else  UART_RX_NUM|=0x80;	
+				}
+				else 
+				{	
+					if(Res==0x0d) UART_RX_NUM|=0x40;
+					else
+					{
+						RxBuffer[ UART_RX_NUM&0X3F]=Res ;
+						UART_RX_NUM++;
+						if( UART_RX_NUM>63) UART_RX_NUM=0;
+					}		 
+				}
+			}  		 
+		}
+}
+*/
--- stm8s_conf.h
+++ stm8s_conf.h
@@ -8,6 +8,8 @@
 #include "stm8s_gpio.h"
 #include "stm8s_clk.h"
 #include "stm8s_tim4.h"
+#include "stm8s_uart1.h"
+#include "stm8s_spi.h"
 
 /* Exported macro ------------------------------------------------------------*/
 #ifdef  USE_FULL_ASSERT
