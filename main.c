bee7a44ec9285e5ea0ce822a375422690d22f81f
Jim Liu <jim.t90615@gmail.com>
2026-01-15 23:23:17 +0800

    stm8: 105: fix rc522 halt on uart begin.
    fix rc522 halt on uart begin, but didn't find the reson
    
    Signed-off-by: Jim Liu <jim.t90615@gmail.com>

--- main.c
+++ main.c
@@ -32,45 +32,14 @@ main()
 	u8 set=0;
 	Clock_Config();
 	GPIO_Init(GPIOE, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_FAST);
-	GPIO_Init(GPIOA, GPIO_PIN_1, GPIO_MODE_OUT_PP_LOW_FAST);
-	GPIO_Init(GPIOA, GPIO_PIN_2, GPIO_MODE_OUT_PP_LOW_FAST);
-	GPIO_Init(GPIOD, GPIO_PIN_7, GPIO_MODE_OUT_PP_LOW_FAST);
-	
-	GPIO_Init(GPIOB, GPIO_PIN_2, GPIO_MODE_OUT_PP_LOW_FAST);
-	GPIO_Init(GPIOB, GPIO_PIN_3, GPIO_MODE_OUT_PP_LOW_FAST);
-	GPIO_Init(GPIOB, GPIO_PIN_1, GPIO_MODE_OUT_PP_LOW_FAST);
-	GPIO_Init(GPIOD, GPIO_PIN_0, GPIO_MODE_OUT_PP_LOW_FAST);
-	GPIO_Init(GPIOD, GPIO_PIN_2, GPIO_MODE_OUT_PP_LOW_FAST);
-	GPIO_Init(GPIOB, GPIO_PIN_0, GPIO_MODE_OUT_PP_LOW_FAST);
-	MX_TIM4_Init();
-	//TIM4_Init();
+
+	TIM4_Init();
 	InitRc522();
 	Uart_Init();
-	rim();
 	UART2_SendString(Tx_Buffer,BufferSize);
-	GPIO_WriteHigh(GPIOB, GPIO_PIN_2);
-	GPIO_WriteHigh(GPIOB, GPIO_PIN_3);
-	GPIO_WriteHigh(GPIOB, GPIO_PIN_1);
-	
-	GPIO_WriteLow(GPIOB, GPIO_PIN_0);
-	GPIO_WriteLow(GPIOD, GPIO_PIN_0);
-	GPIO_WriteLow(GPIOD, GPIO_PIN_2);
-	
+
 	while (1){
-		GPIO_WriteReverse(GPIOE, GPIO_PIN_5);
-		//GPIO_WriteReverse(GPIOA, GPIO_PIN_1);
-		//GPIO_WriteReverse(GPIOA, GPIO_PIN_2);
-		//GPIO_WriteReverse(GPIOD, GPIO_PIN_7);
-		////GPIO_WriteReverse(GPIOC, GPIO_PIN_2);
-		////Delay_ms_int(1000);
-		Delay_ms(500);
-		//GPIO_WriteReverse(GPIOB, GPIO_PIN_2);
-		//GPIO_WriteReverse(GPIOB, GPIO_PIN_3);
-		//GPIO_WriteReverse(GPIOB, GPIO_PIN_1);
-	
-		//GPIO_WriteReverse(GPIOB, GPIO_PIN_0);
-		//GPIO_WriteReverse(GPIOD, GPIO_PIN_0);
-		//GPIO_WriteReverse(GPIOD, GPIO_PIN_2);
+		Delay_ms(100);
 		
 		showcard(Tx_Buffer,&set);
 		Reset_RC522();
